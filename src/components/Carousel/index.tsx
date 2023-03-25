3e58f53668fd7c80823e4c1788f6daa7554ea023
Eliúde Vemba <sweeteliude@gmail.com>
2023-03-25 21:36:36 +0100

    Fix carousel
    And refactor it

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { Tooltip } from 'react-tooltip'
 
 type Props = {
-	data: string[]
+	children: JSX.Element[]
+	id: string
 }
 
-const Carousel = ({ data }: Props) => {
+const Carousel = ({ children, id }: Props) => {
 	const [index, setIndex] = useState(0)
 
 	useEffect(() => {
-		if (data.length < 2) return
+		if (children.length < 2) return
 		let timer: number | undefined
-		if (index < data.length - 1) {
+		if (index <= children.length - 1) {
 			timer = setInterval(() => setIndex(index + 1), 5000)
 		} else {
 			setIndex(0)
@@ -25,48 +25,31 @@ const Carousel = ({ data }: Props) => {
 		<div className='w-full h-full flex flex-col items-center xs:px-10 px-5'>
 			<div className='w-full my-auto flex overflow-hidden'>
 				{
-					data.map((d, i) => (
+					children.map((child, i) => (
 						<div
 							key={i}
-							className='min-w-full flex'
+							className='min-w-full'
 							style={{
 								transform: `translateX(-${index * 100}%)`,
 								transition: 'transform .5s'
 							}}
 						>
-							<p className='text-sm text-center font-bold md:my-auto my-8'>
-								{d}
-							</p>
+							{child}
 						</div>
 					))
 				}
 			</div>
 			<div className='flex gap-4 md:my-auto my-8'>
 				{
-					data.length > 1 ? data.map((d, i) => (
+					children.length > 1 ? children.map((_, i) => (
 						<div
 							key={i}
-							data-tooltip-id={i.toString()}
+							data-tooltip-id={i.toString() + id}
 							className={`
 								h-3 w-3 rounded-[50%] cursor-pointer
 								${i == index ? 'bg-white border-2 border-solid border-black' : 'bg-black'}
 							`}
 							onClick={() => setIndex(i)}
-						/>
-					)) : null
-				}
-				{
-					data.length > 1 ? data.map((d, i) => (
-						<Tooltip
-							key={i}
-							id={i.toString()}
-							content={d.slice(0, 20) + '...'}
-							place='top'
-							style={{
-								fontSize: '10px',
-								padding: '4px',
-								backgroundColor: '#666666'
-							}}
 						/>
 					)) : null
 				}
