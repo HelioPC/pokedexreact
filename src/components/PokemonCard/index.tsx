1bcfb5ad199fcebb593d57b0999213e7fa809bd2
Eliúde Vemba <sweeteliude@gmail.com>
2023-04-20 06:27:01 +0100

    Feat: animate pokemon card

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Tilt } from 'react-tilt'
 
 import { api, BASE_IMAGE_URL } from '../../api'
 import { Pokemon } from '../../types/core'
 import { formatNumber } from '../../helpers/numbers'
 import TypeLabel from '../TypeLabel'
-import { Link } from 'react-router-dom'
 
 type PokemonCardProps = {
 	name: string
@@ -30,35 +31,37 @@ const PokemonCard = ({ name, pokeData }: PokemonCardProps) => {
 
 	return (
 		pokemon ? (
-			<Link to={`/pokemon/${pokemon.id}`} state={{ fromApp: true, pokemon: pokemon }}>
-				<div className='w-full h-full max-w-[200px] rounded-md hover:shadow-xl shadow-lg duration-300 hover:scale-[1.02] cursor-pointer bg-white pb-2'>
-					<div className='w-full flex justify-center h-auto bg-[#f5f5f5] rounded-t-md overflow-hidden'>
-						<img
-							src={`${BASE_IMAGE_URL}${pokemon.id}.png`}
-							className='sm:w-full w-[60%] h-48 hover:scale-105 duration-300'
-							onLoad={() => setImageLoading(false)}
-							onError={() => setTooManyRequests(true)}
-							style={
-								tooManyRequests ? { display: 'none' } :
-									imageLoading ? {} : { display: 'block' }
-							}
-						/>
-					</div>
-					<div className='w-full flex flex-col sm:items-start items-center sm:px-5 px-2 sm:py-2 py-1'>
-						<p className='text-xs font-bold text-[#888]'>Nº: {formatNumber(pokemon.id)}</p>
-						<div className='mt-1'>
-							<p className='text-sm'>{pokemon.name}</p>
+			<Tilt>
+				<Link to={`/pokemon/${pokemon.id}`} state={{ fromApp: true, pokemon: pokemon }}>
+					<div className='w-full h-full max-w-[200px] rounded-md hover:shadow-xl shadow-lg duration-300 cursor-pointer bg-white pb-2'>
+						<div className='w-full flex justify-center h-auto bg-[#f5f5f5] rounded-t-md overflow-hidden'>
+							<img
+								src={`${BASE_IMAGE_URL}${pokemon.id}.png`}
+								className='sm:w-full w-[60%] h-48 duration-300'
+								onLoad={() => setImageLoading(false)}
+								onError={() => setTooManyRequests(true)}
+								style={
+									tooManyRequests ? { display: 'none' } :
+										imageLoading ? {} : { display: 'block' }
+								}
+							/>
 						</div>
-						<div className='flex sm:gap-2 md:gap-1 lg:gap-4 gap-1 mt-2'>
-							{
-								pokemon.types.map((t, i) => (
-									<TypeLabel key={i} name={t.type.name} />
-								))
-							}
+						<div className='w-full flex flex-col sm:items-start items-center sm:px-5 px-2 sm:py-2 py-1'>
+							<p className='text-xs font-bold text-[#888]'>Nº: {formatNumber(pokemon.id)}</p>
+							<div className='mt-1'>
+								<p className='text-sm'>{pokemon.name}</p>
+							</div>
+							<div className='flex sm:gap-2 md:gap-1 lg:gap-4 gap-1 mt-2'>
+								{
+									pokemon.types.map((t, i) => (
+										<TypeLabel key={i} name={t.type.name} />
+									))
+								}
+							</div>
 						</div>
 					</div>
-				</div>
-			</Link>
+				</Link>
+			</Tilt>
 		) : (
 			<div className='hidden' />
 		)
